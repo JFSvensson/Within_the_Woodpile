d5441b77727160c0598ba3273012aa2cc97e813c
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-07 14:52:56 +0200

    Move CollisionManager, CreatureManager, and GameStateManager

---
+++ src/core/managers/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Core managers exports
+ */
+export { GameStateManager } from './GameStateManager.js';
+export { CreatureManager } from './CreatureManager.js';
+export { CollisionManager } from './CollisionManager.js';
