31aa7a97a9070c720cb41b6cc7e017190bdfc666
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-07 15:38:19 +0200

    Add menu renderer components export

---
+++ src/presentation/renderers/menu/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Menu renderer components
+ */
+export { MenuRenderer } from './MenuRenderer.js';
+export { BackgroundRenderer } from './BackgroundRenderer.js';
+export { LogoRenderer } from './LogoRenderer.js';
