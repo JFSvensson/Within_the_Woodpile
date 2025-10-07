2264d43b56d542ac9d6b5af912da518d941d9eda
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-07 15:39:19 +0200

    Add game renderer components export

---
+++ src/presentation/renderers/game/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Game renderer components
+ */
+export { GameRenderer } from './GameRenderer.js';
+export { UIRenderer } from './UIRenderer.js';
+export { WoodPieceRenderer } from './WoodPieceRenderer.js';
