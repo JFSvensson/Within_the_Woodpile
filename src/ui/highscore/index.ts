4c7bb74fd0e81b7f3d1296228772d2691634b835
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-08 18:01:42 +0200

    Add index.ts for exporting highscore UI components

---
+++ src/ui/highscore/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Highscore UI Components
+ * Exporterar alla highscore-relaterade UI-komponenter
+ */
+
+export { HighscoreTable } from './HighscoreTable.js';
+export { AddScoreDialog } from './AddScoreDialog.js';
+export { StatisticsPanel } from './StatisticsPanel.js';
+export { HighscoreModal } from './HighscoreModal.js';
