03b8fd64b34bda0c8d2b34d8dba7e495d2fb3831
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-08 17:47:14 +0200

    Add HighscoreService export to core services index

--- src/core/services/index.ts
+++ src/core/services/index.ts
@@ -4,3 +4,4 @@
 export { WoodPileGenerator } from './WoodPileGenerator.js';
 export { CollapsePredictionCalculator } from './CollapsePredictionCalculator.js';
 export { HighscoreService } from './HighscoreService.js';
+export { HighscoreI18nService } from './HighscoreI18nService.js';
