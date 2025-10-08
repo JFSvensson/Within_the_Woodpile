c5c3a745780034a12fe1fe6ca6120c1d024a1b71
Fredrik Svensson <fs222id@student.lnu.se>
2025-10-08 17:14:07 +0200

    Add export for HighscoreStorageService in storage index

--- src/infrastructure/storage/index.ts
+++ src/infrastructure/storage/index.ts
@@ -3,4 +3,5 @@
  */
 export { LocalStorageService } from './LocalStorageService.js';
 export { GameDataRepository } from './GameDataRepository.js';
+export { HighscoreStorageService } from './HighscoreStorageService.js';
 export type { StorageService, GameData } from './interfaces.js';
