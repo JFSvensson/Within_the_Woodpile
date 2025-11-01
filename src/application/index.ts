b3ca5a88c1e22c93f9d1cde00c36af0257517fae
Fredrik Svensson <svensson.jf@gmail.com>
2025-11-01 21:46:21 +0100

    Add index file for application layer exports

---
+++ src/application/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Application layer exports
+ * Barrel export för clean import paths
+ */
+
+export { AppController } from './AppController.js';
+export { MenuController } from './MenuController.js';
+export { GameController } from './GameController.js';
+export { ModalManager } from './ModalManager.js';
